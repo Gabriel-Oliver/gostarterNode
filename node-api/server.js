e36aaf807a99d3894b8a3980b95a2a551359e45b
Gabriel Óliver <gabrielmed2010@hotmail.com>
2020-08-02 14:45:26 -0300

    connect to the database

--- node-api/server.js
+++ node-api/server.js
@@ -1,7 +1,16 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
+//iniciando o app
 const app = express();
 
+//iniciando o DB
+mongoose.connect("mongodb://localhost:27017/nodeapi", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+//criando primeira rota
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
